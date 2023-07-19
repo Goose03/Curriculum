c7379eb6bffa8cb667a198634f265671cad27934
goose <gusgt03@gmail.com>
2023-07-19 10:26:34 -0600

    Final

--- app/page.js
+++ app/page.js
@@ -1,113 +1,186 @@
-import Image from 'next/image'
+"use client"
+
+import github from '../public/pfp.jpeg';
+import me from '../public/me.jpeg';
+import blob1 from '../public/blob1.svg';
+
+import React, {useState} from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default function Home() {
+  
+  const copyToClipboard = () => {
+    alert(`Copied to Clipboard`);
+}
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen items-center justify-between bg-gradient-to-r from-zeus-400 to-zeus-200">
+      
+      <div className="navbar bg-zeus-900">
+        <div className="flex-1">
+          <p className="px-5 normal-case text-xl text-zeus-50">Mi CV Virtual</p>
+        </div>
+
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 items-center">
+            <li><a className="text-zeus-50" href="https://github.com/Goose03">Github</a></li>
+            
+            <li>
+            <div className="avatar">
+              <div className="w-9 rounded-full">
+                <a href="https://github.com/Goose03"><img src="./pfp.jpeg"/></a>
+              </div>
+            </div>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='grid grid-cols-3 gap-4 py-10 opacity-80'>
+
+        <div className="py-10 bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-r-lg">
+          <figure class="relative w-36 h-36 mx-auto transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+              <img class="rounded-full mx-auto" src="/me.jpeg"></img>
+          </figure>
+
+          <div className='pt-3 text-2xl text-center text-zeus-50'>Gustavo Garcia Tellez</div>
+
+
+          <div className='divider w-2/3 mx-auto'></div>
+
+
+          <div className='pt-3 pb-3 text-xl underline text-center text-zeus-50'>Contactos</div>
+          <div className='flex flex-col place-items-center'>
+            <div className='py-3'>
+              <CopyToClipboard text="gusgt03@gmail.com">
+                <button className="btn btn-active btn-ghost" onClick={copyToClipboard}>gusgt03@gmail.com</button>
+              </CopyToClipboard>
+            </div>
+
+            <div className='py-3 '>
+              <CopyToClipboard text="442-287-4848">
+                <button className=" btn btn-active btn-ghost" onClick={copyToClipboard}>442-287-4848</button>
+              </CopyToClipboard>
+            </div>
+          </div>
+
+
+          <div className='divider w-2/3 mx-auto '></div>
+
+
+          <div className='pt-3 text-xl underline text-center text-zeus-50'>Perfil Profesional</div>
+          <p className='px-6 py-3 text-justify text-zeus-50'> 
+            Estudiante del Tecnológico de Monterrey de Ingenieria en Tecnologías Computacionales, en busca de crecimiento profesional
+            y con ansias de aprender. Interesado en desarrollo web y ciber-seguridad. Dedicado a desarrollar un ambiente igualitario y seguro
+            para todos.
+          </p>
+
+        </div>
+
+
+        <div className="pt-10 mx-6 flex flex-col place-items-center gap-5">
+          <div className='bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-lg w-full'>
+            <div className='pt-3 pb-2 text-xl underline text-center text-zeus-50 font-semibold'>Experiencia Laboral</div>
+            <p className='px-6 pb-1 text-center text-zeus-50 underline decoration-double'>Junior Sales Associate, Rexair Mexico</p>
+            <p className='text-sm text-center text-zeus-50 italic'>Jun 2021 - Jul 2021</p>
+            <ul className='py-3 px-3 text-justify text-sm list-disc list-inside leading-relaxed'>
+              <li>Venta de productos de limpieza de gamma alta</li>
+              <li>Demostraciones a clientes, en las cuales se demostraba funciones y utilidad del producto a vender</li>
+              <li>Reclutacion de empleados para ampliar la fuerza de trabajo</li>
+              <li>Recoleccion de referidos de los clientes que recomendaban mi trabajo</li>
+            </ul>
+          </div>
+
+          <div className='bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-lg w-full'>
+            <div className='pt-3 pb-2 text-xl underline text-center text-zeus-50 font-semibold'>Formación</div>
+
+            <p className='px-6 pb-1 text-center text-zeus-50 underline'>Tecnológico de Monterrey, Campus Guadalajara</p>
+            <ul className='list-inside text-center text-zeus-50'>
+              <li className='list-disc italic font-light'>Ingeneria en Tecnologias Computacionales</li>
+              <li className='font-light text-xs'>Tercer semestre</li>
+            </ul>
+
+            <p className='px-6 pb-1 text-center text-zeus-50 underline'>John F. Kennedy The American School of Queretaro</p>
+            <ul className='pb-3 list-inside list-disc text-center text-zeus-50'>
+              <li className='font-light'>Diploma de preparatoria SEP</li>
+              <li className='font-light'>Diploma de preparatoria Americana</li>
+            </ul>
+          </div>
+
+          <div className='bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-lg w-full'>
+            <div className='pt-3 pb-2 text-xl underline text-center text-zeus-50 font-semibold'>Idiomas</div>
+
+            <div className='px-6 grid grid-cols-3'>
+              <p className='col-start-1 col-end-1'>Español</p>
+              <p className='col-start-3 col-end-3 text-end'>Nativo</p>
+
+              <p className='col-start-1 col-end-1'>Ingles</p>
+              <p className='col-start-3 col-end-3 text-end pb-3'>C2</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="pt-10 mx-6 flex flex-col place-items-center gap-5">
+          <div className='bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-lg w-full'>
+            <div className='pt-3 pb-2 text-xl underline text-center text-zeus-50 font-semibold'>Competencias</div>
+
+            <div className='px-6 grid grid-cols-4'>
+              <div className='col-start-1 col-span-2'><p>Desarrollo en React, Tailwind CSS</p></div>
+              <p className='col-start-4 text-end'>Competente</p>
+            </div>
+
+            <div className='divider w-2/3 mx-auto'></div>
+
+            <div className='px-6 grid grid-cols-4'>
+              <div className='col-start-1 col-span-2'><p>Desarrollo en C++</p></div>
+              <p className='col-start-4 text-end'>Competente</p>
+            </div>
+
+            <div className='divider w-2/3 mx-auto'></div>
+
+            <div className='px-6 grid grid-cols-4'>
+              <div className='col-start-1 col-span-2'><p>Desarrollo en Python</p></div>
+              <p className='col-start-4 text-end'>Principiante</p>
+            </div>
+
+            <div className='divider w-2/3 mx-auto'></div>
+
+            <div className='px-6 grid grid-cols-4'>
+              <div className='col-start-1 col-span-2'><p>Desarrollo en JS</p></div>
+              <p className='col-start-4 text-end'>Principiante</p>
+            </div>
+
+            <div className='divider w-2/3 mx-auto'></div>
+
+            <div className='px-6 grid grid-cols-4'>
+              <div className='col-start-1 col-span-2'><p>Uso de Github</p></div>
+              <p className='col-start-4 text-end'>Competente</p>
+            </div>
+
+            <div className='divider w-2/3 mx-auto'></div>
+
+            <div className='px-6 grid grid-cols-4 pb-3'>
+              <div className='col-start-1 col-span-2'><p>Colaboracion en Equipo </p></div>
+              <p className='col-start-4 text-end'>Competente</p>
+            </div>
+          </div>
+
+
+          <div className='bg-gradient-to-r from-zeus-600 to-zeus-500 rounded-lg w-full'>
+            <div className='pt-3 pb-2 text-xl underline text-center text-zeus-50 font-semibold'>Experiencia no Laboral</div>
+              <p className='px-6 pb-1 text-center text-zeus-50 underline decoration-double'>Hackathon Solana, Super Team Mx</p>
+              <p className='text-sm text-center text-zeus-50 italic'>Jun 2023 - Jun 2023</p>
+              <ul className='py-3 px-3 text-justify text-sm list-disc list-inside leading-relaxed'>
+                <li>Hackathon a nivel nacional centrado en desarrollo de web 3</li>
+                <li>Desarrollo front-end y UI</li>
+              </ul>
+          </div>
+
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -12,7 +13,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'zeus': {
+          '50': '#f6f5f3',
+          '100': '#e9e5e2',
+          '200': '#d4cfc6',
+          '300': '#b5aea0',
+          '400': '#928975',
+          '500': '#736c56',
+          '600': '#5a5441',
+          '700': '#484334',
+          '800': '#3a372b',
+          '900': '#26241c',
+          '950': '#1b1a13',
+        },      
+      }   
     },
   },
-  plugins: [],
+  plugins: [require("daisyui"), require('flowbite/plugin')],
 }
